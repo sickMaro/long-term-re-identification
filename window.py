91ab736980aec84bd50de2d133dc1ca47e477533
sickMaro <146954536+sickMaro@users.noreply.github.com>
2024-05-12 18:39:25 +0200

    added result handling functions and widgets

--- window.py
+++ window.py
@@ -1,5 +1,7 @@
 from tkinter import ttk
 from PIL import Image, ImageTk
+from datetime import datetime
+from itertools import zip_longest
 import os
 import sys
 import tkinter as tk
@@ -9,7 +11,8 @@ import re
 
 
 class Window:
-    __TITLE: str = 'Person re-identification - SATcase6video'
+    __MAIN_TITLE: str = 'Person re-identification - SATcase6video'
+    __RESULT_TITLE: str = 'Result - SATcase6video'
     __PLAY_BUTTON: tk.PhotoImage = None
     __PAUSE_BUTTON: tk.PhotoImage = None
     __VOLUME_BUTTON: tk.PhotoImage = None
@@ -20,12 +23,13 @@ class Window:
     __THUMB_HEIGHT: int = 100
     __SCROLLBAR_WIDTH: int = 20
     __CAMERAS: list[(str, cv2.VideoCapture, tk.PhotoImage)] = []
-    __DIRECTORY: str = "C:/Users/robid/PycharmProjects/pythonProject2/video"
+    __DIRECTORY: str = "C:/Users/robid/PycharmProjects/pythonProject2/"
+    first = 0
 
     def __init__(self) -> None:
 
         self.__window: tk.Tk = tk.Tk()
-        self.__configure_window()
+        self.__configure_window(main=True)
 
         Window.__load_images_and_cameras()
 
@@ -35,17 +39,20 @@ class Window:
         self.__current_frame_PI: tk.PhotoImage = ...
         self.__current_selected_area: tk.PhotoImage | None = None
 
-        self.__frm_video: tk.Frame = tk.Frame(self.__window, bg="light grey")
+        self.__frm_main: tk.Frame = tk.Frame(self.__window, bg="light grey")
+        self.__frm_video: tk.Frame = tk.Frame(self.__frm_main, bg="light grey")
         self.__frm_cameras: tk.Frame = tk.Frame(self.__window, bg="light grey")
 
         # size for the resized frames of the video
         Window.__WIDTH, Window.__HEIGHT = (Window.__get_frm_video_width(self),
                                            Window.__get_frm_video_height(self))
 
-        self.__lbl_title: tk.Label = tk.Label(self.__window, fg="black", bg="light grey", text=Window.__TITLE,
-                                              height=2, relief="raised", font=("Arial", 25))
+        self.__lbl_title: tk.Label = tk.Label(self.__window, fg="black", bg="light grey",
+                                              text=Window.__MAIN_TITLE, height=2, relief="raised",
+                                              font=("Arial", 25))
 
-        self.__cnv_video: tk.Canvas = tk.Canvas(self.__frm_video, height=Window.__HEIGHT, width=Window.__WIDTH)
+        self.__cnv_video: tk.Canvas = tk.Canvas(self.__frm_video, height=Window.__HEIGHT, width=Window.__WIDTH,
+                                                highlightthickness=0)
 
         self.__scrollbar_camera: tk.Scrollbar = tk.Scrollbar(self.__frm_cameras, orient=tk.VERTICAL, bd=20,
                                                              width=Window.__SCROLLBAR_WIDTH, bg="light grey")
@@ -66,9 +73,24 @@ class Window:
                                                      borderwidth=0)
 
         self.__frm_final_cameras: tk.Frame = tk.Frame(self.__canvas_cameras, bg="white")
+        self.__frm_final_results: tk.Frame = tk.Frame(self.__canvas_cameras, bg="white")
+
+        self.__lbl_result_title: tk.Label = tk.Label(self.__frm_final_results, text="Matches",
+                                                     font=("Arial", 13), fg="black", bg="white")
 
         self.__progress_bar: ttk.Progressbar = ttk.Progressbar(self.__frm_video_bar, orient='horizontal',
                                                                mode='determinate')
+
+        self.__btn_identification: tk.Button = tk.Button(self.__frm_video, text="Identify", bg="dark grey",
+                                                         relief="ridge", command=self.__show_results)
+
+        self.__frm_probe: tk.Frame = tk.Frame(self.__frm_main, bg="light grey")
+        self.__lbl_probe_title: tk.Label = tk.Label(self.__frm_probe, text="Your probe", font=("Arial", 13),
+                                                    fg="black", bg="light grey")
+
+        self.__lbl_probe: tk.Label = tk.Label(self.__frm_probe, highlightthickness=0, border=0)
+        self.__btn_change_probe: tk.Button = tk.Button(self.__frm_probe, text="Change probe", bg="dark grey",
+                                                       command=self.__get_back, width=50, relief="ridge")
 
         self.__configure_and_grid_widgets()
 
@@ -77,46 +99,63 @@ class Window:
         self.__bind_events()
         self.__create_cameras_frames()
 
-        self.__frm_final_cameras.update_idletasks()
-        self.__canvas_cameras.config(scrollregion=self.__canvas_cameras.bbox("all"))
-
         self.__start_x: int = -1
         self.__start_y: int = -1
+        self.__results: list[tk.PhotoImage] = []
 
     def __del__(self):
+        if self.__results:
+            self.__results.clear()
+
+        self.__current_frame_PI = None
+        self.__current_selected_area = None
+
         self.__window.destroy()
 
-    def __configure_window(self) -> None:
-        self.__window.state('zoomed')
-        self.__window.resizable(True, True)
+    def __configure_window(self, main: bool = False, multiplier: float = 1.5) -> None:
+        if main:
+            self.__window.state('zoomed')
+            self.__window.resizable(True, True)
 
-        self.__window.rowconfigure(0, weight=0)
-        self.__window.rowconfigure(1, weight=0)
+        self.__window.rowconfigure(1, weight=1)
         self.__window.columnconfigure(0, minsize=self.__window.winfo_screenmmwidth(), weight=1)
-        self.__window.columnconfigure(1, minsize=self.__window.winfo_screenmmwidth() * 1.5, weight=1)
+        self.__window.columnconfigure(1, minsize=self.__window.winfo_screenmmwidth() * multiplier, weight=1)
 
     def __configure_and_grid_widgets(self) -> None:
 
-        Window.__configure_widgets(
-            (self.__frm_video, ((0, 0), (1, 0))),
-            (self.__frm_cameras, ((1,), (1, 0))),
-            (self.__frm_video_bar, ((), (0, 1, 0))),
-            (self.__frm_container, ((1,), (1, 0))),
-            (self.__frm_final_cameras, ((), (0, 1))))
+        Window.__configure_widgets((self.__frm_main, ((0, 0), (1, 0))),
+                                   (self.__frm_video, ((0, 0), (1, 0))),
+                                   (self.__frm_cameras, ((1,), (1, 0))),
+                                   (self.__frm_video_bar, ((), (0, 1, 0))),
+                                   (self.__frm_container, ((1,), (1, 0))),
+                                   (self.__frm_final_cameras, ((1,), (0, 1))),
+                                   (self.__frm_final_results, ((0,), (1, 1, 1, 1, 1))),
+                                   (self.__frm_probe, ((0, 0), (1, 0))))
 
-        self.__frm_video.grid(row=1, column=0, sticky="nsew", padx=(15, 15), pady=35)
+        self.__frm_main.grid(row=1, column=0, sticky="new", padx=(15, 15), pady=35)
+        self.__frm_video.grid(row=0, column=0, sticky="nsew")
         self.__frm_cameras.grid(row=1, column=1, sticky="nsew", padx=(0, 15), pady=35)
-        self.__lbl_title.grid(row=0, column=0, columnspan=2, sticky="new")
+        self.__lbl_title.grid(row=0, column=0, columnspan=2, sticky="new", padx=2, pady=2)
         self.__cnv_video.grid(row=0, column=0, padx=55, pady=(30, 0), sticky="w")
         self.__cnv_video.grid_propagate(False)
+        self.__btn_identification.grid(row=2, column=0, sticky="ew")
         self.__frm_video_bar.grid(row=1, column=0, padx=55, pady=(0, 20), sticky="nwe")
         self.__btn_play_video.grid(row=0, column=0, sticky="nsew")
         self.__btn_video_volume.grid(row=0, column=2, sticky="e")
         self.__frm_container.grid(row=0, column=0, sticky="nsew", padx=10, pady=10)
         self.__canvas_cameras.grid(row=0, column=0, sticky="nsew")
         self.__frm_final_cameras.grid(row=0, column=0, sticky="nsew")
+        self.__frm_final_results.grid(row=0, column=0, sticky="nsew")
+        self.__lbl_result_title.grid(row=0, column=0, sticky="ew", pady=(10, 30), columnspan=5)
+        self.__frm_final_results.grid_remove()
         self.__progress_bar.grid(row=0, column=1, sticky="ew")
         self.__scrollbar_camera.grid(row=0, column=1, rowspan=2, sticky="nse")
+
+        self.__frm_probe.grid(row=0, column=0, sticky="nsew")
+        self.__lbl_probe_title.grid(row=0, column=0, sticky="ew", pady=10)
+        self.__lbl_probe.grid(row=1, column=0, sticky="n", padx=15)
+        self.__btn_change_probe.grid(row=2, column=0, sticky="n", pady=20, padx=0)
+        self.__frm_probe.grid_remove()
 
     def __canvas_configure(self, event: tk.Event) -> None:
         self.__canvas_cameras.itemconfig("cameras", width=event.width)
@@ -131,8 +170,9 @@ class Window:
                 cls.__VOLUME_BUTTON = ImageTk.PhotoImage(Image.open("images/volume.png").resize((30, 30)))
 
             if not cls.__CAMERAS:
-                for file in os.listdir(cls.__DIRECTORY):
-                    path = os.path.join(cls.__DIRECTORY, file)
+                pt = cls.__DIRECTORY + "video"
+                for file in os.listdir(pt):
+                    path = os.path.join(pt, file)
                     if os.path.isfile(path) and file.endswith(cls.__VIDEO_TYPE):
                         video = cv2.VideoCapture(path)
                         if video.isOpened():
@@ -210,7 +250,8 @@ class Window:
         self.__progress()
 
     @staticmethod
-    def __configure_widgets(*widgets: tuple[tk.Misc, tuple[tuple[int | None, ...], ...]]) -> None:
+    def __configure_widgets(*widgets: tuple[tk.Misc, tuple[tuple[int | None, ...], tuple[int | None, ...]]]) \
+            -> None:
         for widget, (rows, columns) in widgets:
             if rows is not None:
                 for i, weight in enumerate(rows):
@@ -227,10 +268,9 @@ class Window:
             video_canvas = tk.Canvas(self.__frm_final_cameras,
                                      height=Window.__THUMB_HEIGHT, width=Window.__THUMB_WIDTH)
             if i == 0:
-                video_canvas.grid(row=i, column=0, padx=pad_x, pady=pad_y + pad_y / 2, sticky="nw")
                 self.__update_main_video(video)
-            else:
-                video_canvas.grid(row=i, column=0, padx=pad_x, pady=pad_y, sticky="nw")
+
+            video_canvas.grid(row=i, column=0, padx=pad_x, pady=pad_y, sticky="nw")
 
             video_canvas.create_image((0, 0), anchor=tk.NW, image=photo)
 
@@ -241,6 +281,70 @@ class Window:
 
             lbl_camera_title = tk.Label(self.__frm_final_cameras, text=camera_title, font=("Arial", 13), bg="white")
             lbl_camera_title.grid(row=i, column=1)
+
+        self.__frm_final_cameras.update_idletasks()
+        self.__canvas_cameras.config(scrollregion=self.__canvas_cameras.bbox("all"))
+
+    def __create_results_frames(self):
+        pt: str = self.__DIRECTORY + "results"
+        pattern: str = r'(SAT_Camera\d+)_MILLIS_(\d+)_TRK-ID_(\d+)_TIMESTAMP_(\d+-\d+-\d+)'
+        children_list: list[tk.Widget] = self.__frm_final_results.winfo_children()
+        try:
+            for i, (file, child) in enumerate(zip_longest(os.listdir(pt), children_list[1:])):
+                if file is None:
+                    child.destroy()
+                    children_list.remove(child)
+                else:
+                    path = os.path.join(pt, file)
+                    if os.path.isfile(path):
+                        result = re.search(pattern, file)
+                        img = ImageTk.PhotoImage(Image.open(path))
+                        self.__results.append(img)
+                        number = "N: " + str(i + 1)
+                        id_ = "ID: " + result.group(3)
+                        camera = "CAM: " + result.group(1)
+                        time = "TIME: " + datetime.strptime(result.group(4), "%H-%M-%S").strftime("%H:%M:%S")
+
+                        if child is None:
+                            Window.__create_result_frame(self.__frm_final_results, img, i,
+                                                         number, id_, camera, time)
+                        else:
+                            Window.__update_result_frames(child, img, number, id_, camera, time)
+
+            self.__frm_final_results.update_idletasks()
+            self.__canvas_cameras.config(scrollregion=self.__canvas_cameras.bbox("all"))
+
+        except OSError:
+            print("Error while retrieving results...")
+            self.__on_closing()
+
+    @staticmethod
+    def __create_result_frame(window_: tk.Widget, img: tk.PhotoImage, frm_number: int, *texts: str) -> None:
+        frm_result = tk.Frame(window_, bg="white")
+
+        cnv_result = tk.Canvas(frm_result, width=img.width(), height=img.height(), highlightthickness=0)
+        cnv_result.create_image(0, 0, anchor=tk.NW, image=img)
+        cnv_result.grid(row=0, column=0, sticky="n", padx=(30, 0))
+
+        for i, text in enumerate(texts):
+            lbl_ = Window.__create_result_frame_label(master=frm_result, text=text)
+            lbl_.grid(row=i+1, column=0, sticky="ew", padx=(30, 0))
+
+        frm_result.grid(row=(frm_number // 5) + 1, column=frm_number % 5, sticky="nsew", pady=(0, 20))
+        window_.winfo_children().append(frm_result)
+
+    @staticmethod
+    def __create_result_frame_label(*, master: tk.Widget = None, fg: str = "black", bg: str = "white",
+                                    font: tuple[str, int] = ("Arial", 8), text: str = "") -> tk.Label:
+        return tk.Label(master, fg=fg, bg=bg, font=font, text=text)
+
+    @staticmethod
+    def __update_result_frames(child, img: tk.PhotoImage, *new_text: str) -> None:
+        inner_frm_children_list = child.winfo_children()
+        inner_frm_children_list[0].config(width=img.width(), height=img.height())
+        inner_frm_children_list[0].create_image(0, 0, anchor=tk.NW, image=img)
+        for i, text in enumerate(new_text):
+            inner_frm_children_list[i + 1].config(text=text)
 
     def __update_main_video(self, video: cv2.VideoCapture) -> None:
         self.__paused = True
@@ -272,16 +376,16 @@ class Window:
             darkened_img = cv2.convertScaleAbs(self.__current_frame, alpha=alpha)
             darkened_img[y0:y1, x0:x1] = self.__current_frame[y0:y1, x0:x1]
 
-            photo = ImageTk.PhotoImage(image=Image.fromarray(darkened_img))
+            image = Image.fromarray(darkened_img)
+            photo = ImageTk.PhotoImage(image=image)
 
             self.__cnv_video.create_image((0, 0), anchor=tk.NW, image=photo)
-            self.__current_selected_area = photo
+            self.__current_frame_PI = photo
+            self.__current_selected_area = ImageTk.PhotoImage(image.crop((x0, y0, x1, y1)))
 
     @classmethod
     def __free_resources_on_closure(cls) -> None:
-        for image in (cls.__VOLUME_BUTTON, cls.__PAUSE_BUTTON, cls.__PLAY_BUTTON):
-            if image:
-                image.__del__()
+        (cls.__VOLUME_BUTTON, cls.__PAUSE_BUTTON, cls.__PLAY_BUTTON) = None, None, None
 
         if cls.__CAMERAS:
             for _, video, thumbnail in cls.__CAMERAS:
@@ -298,6 +402,33 @@ class Window:
 
     def show_window(self) -> None:
         self.__window.mainloop()
+
+    def __show_results(self):
+        if self.__current_selected_area is not None:
+            self.__lbl_title.config(text=Window.__RESULT_TITLE)
+            self.__configure_window(multiplier=1.8)
+            self.__frm_video.grid_remove()
+            self.__frm_probe.grid()
+            self.__lbl_probe.config(image=self.__current_selected_area)
+            self.__canvas_cameras.delete("cameras")
+            self.__canvas_cameras.create_window((0, 0), anchor=tk.NW, window=self.__frm_final_results, tags="result")
+            self.__canvas_cameras.bind("<Configure>",
+                                       lambda event: self.__canvas_cameras.itemconfig("result",
+                                                                                      width=event.width))
+
+            self.__create_results_frames()
+
+    def __get_back(self):
+        self.__lbl_title.config(text=Window.__MAIN_TITLE)
+        self.__configure_window()
+        self.__frm_probe.grid_remove()
+        self.__frm_video.grid()
+        self.__canvas_cameras.delete("result")
+        self.__canvas_cameras.create_window((0, 0), anchor=tk.NW, window=self.__frm_final_cameras, tags="cameras")
+        self.__canvas_cameras.bind("<Configure>", self.__canvas_configure)
+        self.__current_selected_area = None
+        if self.__results:
+            self.__results.clear()
 
 
 window = Window()
